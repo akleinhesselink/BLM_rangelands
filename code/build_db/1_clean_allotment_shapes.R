9ffb8d651b3094794234702ba4128000cdb90239
andy <andy.kleinhesselink@gmail.com>
2022-06-16 13:11:08 -0700

    Rewriting allotment cleaning script
    Former-commit-id: 04ee74240522622f4027f7f4cc26a25144ab83f0

--- code/build_db/1_clean_allotment_shapes.R
+++ code/build_db/1_clean_allotment_shapes.R
@@ -8,153 +8,281 @@ m2_per_ACRE <- 4046.8564224
 m2_per_ha <- 1e4
 ACRE_per_ha <- 2.47105
 
-BLM <- sf::read_sf('data/spatial/BLM_National_Grazing_Allotments/gra.gdb/', 
-                   layer = 'gra_allot_poly')
+mdate <- date( file.info('data/spatial/BLM_National_Grazing_Allotments_2022/gra.gdb/a00000001.gdbindexes')$mtime )
 
-n_original <- nrow(BLM)
+allotment_info <- sf::read_sf('data/spatial/BLM_National_Grazing_Allotments_2022/gra.gdb/', 
+                   layer = 'gra_allot_poly') %>% 
+  st_drop_geometry()
 
-BLM <- 
-  BLM %>%
-  mutate( ALLOT_NAME = str_to_upper(str_squish(str_trim(ALLOT_NAME)))) %>% 
-  mutate( ALLOT_NAME = str_remove( ALLOT_NAME, '\\.')) %>%
-  mutate( ALLOT_NAME = str_replace( ALLOT_NAME, 'CR$', 'CREEK')) %>%  # for flying my buffalo creek 
-  arrange( ADMIN_ST, ADM_OFC_CD, ALLOT_NAME, ALLOT_NO) %>% 
-  mutate( uid = row_number() )  %>% 
-  mutate( ushape = as.numeric(as.factor(as.character( SHAPE )))) %>% 
-  mutate( LAST_DATE = date(last_edited_date )) %>%
-  select(uid, ushape, ADMIN_ST, ADM_OFC_CD, ALLOT_NO, ALLOT_NAME, LAST_DATE, SHAPE_Area) 
+sort( unique( allotment_info$ADMIN_ST ) )
 
-temp_allotment_table <- BLM %>% st_drop_geometry()
+allotment_info <- allotment_info %>% 
+  mutate( ADMIN_ST = ifelse( ADMIN_ST == ' ', str_extract(ST_ALLOT, '^[A-Z]{2}'), ADMIN_ST)) %>% 
+  mutate( ST_ALLOT_new = ifelse(
+    !str_extract(ST_ALLOT, '^[A-Z]{2}') %in% c('AZ', 'CA', 'CO', 'ID', 'MT', 'NM', 'NV', 'OR', 'UT', 'WY') ,
+    paste0( ADMIN_ST, ALLOT_NO ), ST_ALLOT ))  
 
-# View duplicate shapes 
-temp_allotment_table %>% 
-  group_by( ushape) %>% 
-  mutate( n_dup = n()) %>% 
-  filter( n_dup > 1 )
-
-# View duplicate names 
-# Some are multipolygons with non-overlapping shapes 
-# Some are different allotments but with same name
-# And some are duplicates of the same allotment with 
-# different boundaries
-temp_allotment_table %>% 
-  group_by(ADMIN_ST, ADM_OFC_CD, ALLOT_NAME ) %>% 
-  mutate( n_dup = n() ) %>% 
-  filter( n_dup > 1 ) %>% nrow()
+allotment_info <- allotment_info %>% 
+  mutate( ST_ALLOT_new = ifelse( !str_detect( ST_ALLOT_new, "[0-9]{5}$" ), 
+                                 paste0( ADMIN_ST, str_pad( ALLOT_NO, 5, side = "left", pad = 0)), 
+                                 ST_ALLOT_new))  
 
 
-# Keep only first record of duplicate polygons 
-# Drops 9 polygons 
-BLM <- 
-  BLM %>% 
-  group_by( ushape) %>% 
-  arrange( ushape, desc(LAST_DATE)) %>% 
-  filter( row_number() ==  1 ) 
+all( str_length(allotment_info$ST_ALLOT_new) == 7 )
 
-n_original - ( BLM %>% nrow() )
+allotment_info <- 
+  allotment_info %>% 
+  ungroup() %>% 
+  mutate( ALLOT_NAME_new = str_trim( str_squish( str_to_upper(str_remove_all(ALLOT_NAME, "[\\.]+" ))))) %>% 
+  arrange( ST_ALLOT_new, last_edited_date) %>% 
+  mutate( ID = as.numeric( factor( paste(ST_ALLOT_new, ALLOT_NAME_new, ADMIN_ST, ADM_UNIT_CD, ADM_OFC_CD)))) 
 
-# Calculate Area 
-BLM <- 
-  BLM %>%
-  ungroup() %>%
-  st_cast("MULTIPOLYGON") %>%
-  st_sf() %>% 
-  mutate( area = st_area(SHAPE) %>% as.numeric()) %>% 
-  mutate( hectare = area/m2_per_ha) 
+allotments <- 
+  allotment_info %>% 
+  left_join( sf::read_sf('data/spatial/BLM_National_Grazing_Allotments_2022/gra.gdb/') %>% 
+               select( SHAPE, GlobalID) %>% 
+               st_transform( crs = 'epsg:5070'), 
+           by = 'GlobalID')
 
-# drop really small polygons
-# less than one hectare 
-# drop 55 allotments 
-BLM %>% 
-  filter( hectare < 1 ) %>% 
-  nrow() 
+allotments <- allotments %>% 
+  st_as_sf() %>% 
+  st_cast( 'MULTIPOLYGON') %>% 
+  filter( !st_is_empty(SHAPE)) %>% 
+  st_make_valid()
 
-BLM <- BLM %>% filter( hectare > 1 )
-
-# Example of redundant allotment 
-BLM %>%
-  filter( ALLOT_NAME == 'WHISKEY RIDGE') %>%
-  mutate( uid = factor(paste( uid, LAST_DATE, sep = '--'))) %>%
-  ggplot() + 
-  geom_sf(aes( fill = uid , color = uid), alpha = 0.2) + 
-  ggtitle('Whiskey Ridge Allotment, MT')+ 
-  theme_bw() + 
-  ggsave('output/figures/Whiskey_Ridge_Duplicate_example.png', 
-         height = 6, width = 8, units = 'in')
+# Group by allot number and name 
+# assign unique ID
+st_write( allotments %>% select( ID, last_edited_date, ALLOT_NAME_new, ST_ALLOT_new, ADMIN_ST, ADM_OFC_CD, ADM_UNIT_CD) , 
+          dsn = 'data/temp/allotments/step_one.shp', 
+          layer = 'step_one', append = F)
 
 
-# Read in BLM districts and populate missing allotment ADMIN_ST field 
-# with ADMIN_ST from BLM district shapefile 
-# Spatial Join allotment centers within admin_dist
+readline(prompt="
+# Read into QGIS and clean with GRASS 
+# 1. Union/v.clean with GRASS
+# 2. Fix geometries 
+# 3. Remove duplicates 
+# 4. Combine features by ID
+# 5. Dissolve internal boundaries with same ID
+# press [Enter] when done to complete R script.")
+
+allotments_clean <- sf::read_sf('data/temp/allotments/step_two.shp') # read in cleaned shapefile
+
+# Read in BLM districts and offices
+# Use spatial join to find missing 
+# states, districts and offices in the allotment data 
 st_layers('data/spatial/BLM_National_Administrative_Units/admu.gdb/' )
 
+mdate_admin_boundaries <- file.info('data/spatial/BLM_National_Administrative_Units/admu.gdb/a00000001.gdbindexes')$mtime
+mdate_admin_boundaries <- date( mdate_admin_boundaries)
+
 districts <- st_read( 'data/spatial/BLM_National_Administrative_Units/admu.gdb/', 
-         layer = 'blm_natl_admu_dist_poly_webpub')
+                      layer = 'blm_natl_admu_dist_poly_webpub') %>% 
+  st_cast('MULTIPOLYGON') %>% 
+  st_make_valid()  %>% 
+  st_transform( crs = 'epsg:5070')
 
-districts <- districts %>% st_transform(crs = 'epsg:5070') 
-districts <- districts %>% select( ADMIN_ST, Shape)
+offices <- st_read('data/spatial/BLM_National_Administrative_Units/admu.gdb/', 
+                   layer = 'blm_natl_admu_field_poly_webpub') %>%  
+  st_cast('MULTIPOLYGON') %>% 
+  st_make_valid() %>% 
+  st_transform( crs = 'epsg:5070')
 
-allotment_centers <- 
-  BLM %>% 
-  st_transform(crs = 'epsg:5070') %>% 
-  group_by( uid ) %>% 
-  summarise( SHAPE = st_centroid(SHAPE) )
+# Add Las Cruces District/Office: missing from field office Layer
+# but is present in District layer 
+Las_Cruces <- districts %>% 
+  filter( ADMIN_ST == 'NM') %>% 
+  filter( PARENT_CD == 'NML00000') 
 
-allotment_centers <- 
-  allotment_centers %>% 
-  st_join( districts, join = st_within , left = T) 
+# Patch whole in Paria River district/Kanab Field office 
+# Hole in Kanab office where Grand staircase escalante is 
+Paria_River <- districts %>% 
+  filter( ADMIN_ST == 'UT' ) %>% 
+  filter( PARENT_CD == 'UTP00000')
 
-uid_with_states <- 
-  allotment_centers %>% 
+offices <- offices %>% 
+  rbind( 
+    Las_Cruces %>%
+      mutate( ADM_UNIT_CD = PARENT_CD) %>%
+      mutate( ADMU_NAME = PARENT_NAME) %>%
+      mutate( BLM_ORG_TYPE = 'Field' ) %>% 
+      mutate( APPRV_DT = NA, EFF_DT = NA, ADMU_ST_URL = NA) ) %>% 
+  rbind( Paria_River %>% 
+           mutate( ADM_UNIT_CD = 'UTP02000' ) %>% 
+           mutate( ADMU_NAME = 'KANAB') %>%
+           mutate( BLM_ORG_TYPE = 'Field') %>% 
+           mutate( APPRV_DT = NA, EFF_DT = NA, ADMU_ST_URL = NA))
+
+
+# Standardize admin names 
+# A couple of ADMU_UNIT_CD's have more than one entry
+# group by ADMU_UNIT_CD and take top row within group (1 per group)
+
+offices_fix_names <- 
+  offices %>% 
+  mutate( ADMU_NAME = str_trim( str_remove(str_to_upper(str_trim(str_squish(ADMU_NAME))), 'FIELD OFFICE|(OFFICE)')), 
+          PARENT_NAME = str_trim( str_remove( str_to_upper( str_trim(str_squish( PARENT_NAME))), '(DISTRICT OFFICE)|(DISTRICT)|(OFFICE)'))) %>% 
+  select( ADM_UNIT_CD, ADMU_NAME, PARENT_CD, PARENT_NAME, ADMIN_ST)  %>% 
+  mutate( ADMU_NAME = paste( ADMU_NAME, 'FIELD OFFICE'), 
+          PARENT_NAME = paste( PARENT_NAME, 'DISTRICT')) 
+
+offices_fix_names <- 
+  offices_fix_names %>% 
+  group_by(ADM_UNIT_CD, ADMU_NAME , PARENT_CD, PARENT_NAME, ADMIN_ST) %>% 
+  summarise( Shape = st_union( Shape))
+
+offices_fix_names %>% 
+  st_cast('MULTIPOLYGON') %>% 
+  st_write('data/temp/BLM_field_offices_cleaned/field_offices.shp', append = F)
+
+office_info <- offices_fix_names %>% st_drop_geometry()
+
+allotments_clean <- 
+  allotments_clean %>% 
+  left_join( office_info, by = c('ADM_UNI'= 'ADM_UNIT_CD')) 
+
+centers_missing_admin_info <- allotments_clean %>%  
+  filter( is.na(ADMU_NAME)) %>% 
+  st_centroid()
+
+spatial_join_admin <- 
+  centers_missing_admin_info %>% 
+  select( - c(ADMU_NAME:ADMIN_ST)) %>% 
+  st_join( offices_fix_names, by = st_within() ) 
+
+allotments_clean <- 
+  allotments_clean %>% 
+  left_join(spatial_join_admin %>% st_drop_geometry(), 
+            by = c('fid', 'cat', 'ID'), suffix = c('', '.X')) %>% 
+  mutate( ADMU_NAME = ifelse( is.na(ADMU_NAME), ADMU_NAME.X, ADMU_NAME)) %>% 
+  mutate( PARENT_CD = ifelse( is.na(PARENT_CD), PARENT_CD.X, PARENT_CD)) %>% 
+  mutate( PARENT_NAME = ifelse( is.na(PARENT_NAME), PARENT_NAME.X, PARENT_NAME)) %>% 
+  mutate( ADMIN_ST = ifelse( is.na(ADMIN_ST), ADMIN_ST.X, ADMIN_ST)) %>% 
+  mutate( ADM_UNI = ifelse( ADM_UNI == 'UTP00000', ADM_UNIT_CD, ADM_UNI)) %>% 
+  mutate( ADM_UNI = ifelse( is.na(ADM_UNI), ADM_UNIT_CD, ADM_UNI)) %>%
+  select( fid:ADMIN_ST) %>% 
+  ungroup()
+
+allotments_clean <- allotments_clean %>% 
+  rename( 'ALLOT_NAME' = ALLOT_N, 
+          'ALLOT_NO' = ST_ALLO, 
+          'ADM_OFC_CD' = ADM_OFC, 
+          'ADM_UNIT_CD' = ADM_UNI, 
+          'OFFICE' = ADMU_NAME, 
+          'DISTRICT' = PARENT_NAME, 
+          'DISTRICT_CD' = PARENT_CD) %>% 
+  select( -fid, -cat, -lst_dt_, - ADMIN_S) %>% 
+  mutate( hectares = as.numeric(st_area(geometry))/m2_per_ha) %>% 
+  filter( hectares > 1) %>% 
+  arrange(ID) %>% 
+  mutate( uname = row_number())
+
+# Join RAS data 
+RAS_info <- read_csv('data/RAS_data/Allotment Information Report-All Allotments.csv') %>% 
+  mutate( `Allotment Number` = str_squish(str_trim(`Allotment Number`)))  %>% 
+  rename( 'Field Office'  = `Field Office...9`) %>% 
+  select( - `Field Office...10`)
+
+RAS_info <- 
+  RAS_info %>% 
+  distinct( `Allotment Number`, `Allotment Name`, `Public Acres`, `Authorization Number`, `Admin State`, `Admin Office`) %>% 
+  group_by( `Allotment Number`, `Allotment Name` ) %>% 
+  arrange( desc( `Authorization Number`)) %>% 
+  filter( row_number() == 1 )
+
+allotments_clean <- 
+  allotments_clean %>% 
+  left_join(RAS_info, by = c('ALLOT_NO' = 'Allotment Number'))
+
+# Add Ecoregion information 
+# input -------------------------- # 
+ER <- read_sf('data/spatial/us_eco_l3_state_boundaries/us_eco_l3_state_boundaries.shp')
+#--------------------------------- # 
+
+ctrs <- allotments_clean %>% st_centroid() %>% 
+  select( uname)
+# add ecoregion column to allotment table: 
+# Get ecoregion
+ER <- ER %>% 
+  st_transform(crs = st_crs(ctrs))
+# 
+ctrs <- ctrs %>% 
+  st_join(ER, left = T, join = st_within )
+
+# Custom Ecoregion classification, keep n similar between ecoregions 
+# Use Level I Ecoregions in all cases except: 
+# 1. Break deserts to level II ecoregions: warm deserts and cold deserts
+# 2. Lump Southern Semi-Arid Highlands with Temperate Sierras = "S. Mts."
+# 3. Split Cold Deserts (ERII) into West and East  
+# 4. Split Great Plains (I) into Northern and Southern 
+
+ctrs <- 
+  ctrs %>%
   st_drop_geometry() %>% 
-  distinct() %>% 
-  rename("CTR_STATE" = ADMIN_ST)
+  mutate( ecoregion = NA_L1NAME ) %>% 
+  mutate( ecoregion = 
+            ifelse( ecoregion %in% 
+                      c('TEMPERATE SIERRAS', 
+                        'SOUTHERN SEMI-ARID HIGHLANDS'), 
+                    'AZ/NM HIGHLANDS', 
+                    ecoregion)) %>%
+  mutate( ecoregion =  
+            ifelse( ecoregion == 'NORTH AMERICAN DESERTS', 
+                    NA_L2NAME, 
+                    ecoregion))  %>%
+  mutate( ecoregion = 
+            ifelse( ecoregion == "COLD DESERTS",
+                    NA_L3NAME,
+                    ecoregion)) %>% 
+  mutate( ecoregion = 
+            ifelse( ecoregion %in% 
+                      c('Wyoming Basin', 
+                        'Colorado Plateaus', 
+                        'Arizona/New Mexico Plateau'),
+                    'E COLD DESERTS', 
+                    ecoregion)) %>% 
+  mutate( ecoregion = 
+            ifelse( ecoregion %in% 
+                      c( 'Central Basin and Range', 
+                         'Northern Basin and Range', 
+                         'Columbia Plateau', 
+                         'Snake River Plain'),
+                    'W COLD DESERTS', 
+                    ecoregion)) %>% 
+  mutate( ecoregion = 
+            ifelse( ecoregion == 'GREAT PLAINS', 
+                    NA_L2NAME, 
+                    ecoregion)) %>% 
+  mutate( ecoregion = 
+            ifelse( ecoregion %in% 
+                      c('WEST-CENTRAL SEMI-ARID PRAIRIES', 'TEMPERATE PRAIRIES'), 
+                    'N GREAT PLAINS', 
+                    ecoregion)) %>% 
+  mutate( ecoregion = 
+            ifelse( ecoregion == 'SOUTH CENTRAL SEMI-ARID PRAIRIES', 
+                    'S GREAT PLAINS', 
+                    ecoregion))  %>% 
+  mutate( ecoregion = 
+            ifelse( ecoregion == 'NORTHWESTERN FORESTED MOUNTAINS', 'Forested Mts', ecoregion))
 
-BLM <- 
-  BLM %>% 
-  mutate( ADMIN_ST = str_trim(str_squish( ADMIN_ST))) %>% 
-  mutate( ADMIN_ST = ifelse( ADMIN_ST == '' , NA, ADMIN_ST)) %>% 
-  left_join(uid_with_states, by = 'uid') %>%
-  mutate( ADMIN_ST = ifelse( is.na( ADMIN_ST), CTR_STATE, ADMIN_ST)) 
 
-BLM <- 
-  BLM %>% 
-  st_transform('epsg:5070') %>%
-  ungroup() %>% 
-  mutate( uname = as.numeric(as.factor(paste0( ADMIN_ST, ADM_OFC_CD, ALLOT_NAME, ALLOT_NO)))) %>% 
-  group_by(uname, ADMIN_ST, ADM_OFC_CD, ALLOT_NO, ALLOT_NAME) %>% 
-  summarise( SHAPE = st_union(SHAPE), LAST_DATE = max(LAST_DATE), FIRST_DATE = min(LAST_DATE) )  %>% 
-  ungroup() %>%
-  st_make_valid() %>%
-  mutate( area = as.numeric( st_area( SHAPE))) %>% 
-  mutate( hectares = as.numeric(area/m2_per_ha))
+allotment_ecoregions <- 
+  ctrs %>%
+  mutate( ecoregion = stringr::str_to_title(ecoregion)) %>%
+  mutate( ecoregion = ifelse( ecoregion == 'Az/Nm Highlands' , 'AZ/NM Highlands',  ecoregion )) %>%
+  select( uname, ecoregion, US_L3CODE:L1_KEY) 
 
-# Check for duplicates 
-stopifnot(
-  BLM %>% 
-  ungroup() %>% 
+allotments_clean <- 
+  allotments_clean %>% 
+  left_join(allotment_ecoregions %>% 
+              select( ecoregion , contains('NAME'), -STATE_NAME), by = 'uname') %>% 
+  st_make_valid() %>% 
+  select( uname, ALLOT_NAME:NA_L1NAME)
+
+allotments_clean   %>% 
   st_drop_geometry() %>% 
-  group_by( uname ) %>% 
-  filter( n() > 1 ) %>%nrow() == 0 )
+  write_csv('data/temp/allotment_info.csv')
 
-
-# Histogram 
-BLM %>% 
-  ggplot( aes( x = hectares )) + 
-  geom_histogram() + 
-  scale_y_continuous(name = 'Number of Allotments') + 
-  scale_x_log10(name = 'Allotment Size (Hectares)', 
-                breaks = c(10, 40, 100, 1000, 10000, 100000), label = scales::comma) + 
-  theme_bw() + 
-  theme( panel.grid = element_blank()) + 
-  ggsave('output/figures/allotment_hectares_histogram.png')
-
-
-# create if don't exit 
-dir.create('data/temp/BLM_allotments_cleaned')
-
-BLM %>% st_write(dsn = 'data/temp/BLM_allotments_cleaned/allotments.shp', 
-                              layer = 'allotments', append = F)
-
-#BLM %>% saveRDS(file = 'data/temp/BLM_allotments_sf.rds')
-
+allotments_clean %>% 
+  st_write( 'data/temp/allotments/allotments_clean.shp', 
+            layer = 'allotments_clean', append = F)
