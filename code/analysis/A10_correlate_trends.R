d6db6ab94a8d72944c8e795d877bb67a5a2aec3c
andy <andy.kleinhesselink@gmail.com>
2022-06-26 17:23:15 -0700

    script for multiscale variance analysis and correlation analysis.  Output of model fitting for multi-scale pixel level trends model
    Former-commit-id: 6a678da8e51cdc4b360b9c71a4eaaaccc7c79859

--- code/analysis/A10_correlate_trends.R
+++ code/analysis/A10_correlate_trends.R
@@ -7,28 +7,292 @@ library(gridExtra)
 source('code/analysis/functions.R')
 source('code/analysis/parameters.R')
 
-# Functions ---------------------- # 
-format_ranefs <- function( x , type = 'AFG'){ 
+format_ranef_trends <- function( model, type, model_scale){ 
+
+  re_df <- ranef(model)
+  scales <- names(re_df)
   
-  x$uname <- 
-    x$uname %>% 
-    mutate( Group = rownames(.) ) %>% 
-    mutate( scale = 'Allotment' , type = type )
+  helper_scale_fun <- function( temp_df, type, scale ) { 
+    
+    if( any(names(temp_df) == 'year2') ) { 
+      temp_df <- temp_df %>% 
+        rename( 't' = year2 )
+    }
+    
+
+    temp_df %>% 
+      mutate( scale = scale) %>% 
+      mutate( ID = rownames(.)) %>% 
+      mutate( type = type ) %>% 
+      rename( "trend" = t )  %>% 
+      select( trend, scale, ID, type )
+  }
   
-  x$`ecogroup:office_label` <- 
-    x$`ecogroup:office_label` %>% 
-    mutate( Group = rownames(.) ) %>% 
-    mutate( scale = 'Office' , type = type )
+   
+  out <- mapply(FUN = helper_scale_fun, temp_df = re_df, type = type, scale = scales, SIMPLIFY = F) %>% 
+    do.call( rbind, . ) %>% 
+    mutate( model_scale = model_scale )
   
-  return(x)
-} 
+  return(out)
+
+}
+
+m_afg <- read_rds('data/temp/afg_pixel_trends_model_extraction_n4.rds')
+m_pfg <- read_rds('data/temp/pfg_pixel_trends_model_extraction_n4.rds')
+m_bgr <- read_rds('data/temp/bgr_pixel_trends_model_extraction_n4.rds')
+m_shr <- read_rds('data/temp/shr_pixel_trends_model_extraction_n4.rds')
+
+afg_ranef <- format_ranef_trends(m_afg, type = 'Annual', model_scale = 'Pixel')  
+bgr_ranef <- format_ranef_trends(m_bgr, type = 'Bare', model_scale = 'Pixel')  
+pfg_ranef <- format_ranef_trends(m_pfg, type = 'Perennial', model_scale = 'Pixel')
+shr_ranef <- format_ranef_trends(m_shr, type = 'Shrub', model_scale = 'Pixel')  
+
+make_pairwise <- function( ranef1, ranef2){ 
+  n1 <- unique(ranef1$type)[1]
+  n2 <- unique(ranef2$type)[1]
+  ranef1 %>% 
+    left_join(ranef2, by = c('scale', 'ID', 'model_scale')) %>% 
+    mutate( pair = paste0( c(n1,n2), collapse = '_')) %>% 
+    filter( complete.cases(.))
+}
+
+# six pairs 
+afg_pfg <- make_pairwise(afg_ranef, pfg_ranef)
+afg_bgr <- make_pairwise(afg_ranef, bgr_ranef)
+afg_shr <- make_pairwise(afg_ranef, shr_ranef)
+bgr_pfg <- make_pairwise(bgr_ranef, pfg_ranef)
+bgr_shr <- make_pairwise(bgr_ranef, shr_ranef)
+pfg_shr <- make_pairwise(pfg_ranef, shr_ranef)
+
+cortest_pairs <- function( pair_df  ) { 
+  
+  pairNames <- str_split( unique( pair_df$pair ), pattern = '_')[[1]] 
+  
+  res <- pair_df %>% 
+    group_by( scale, pair, model_scale) %>% 
+    summarise( corRes = list(cor.test(trend.x, trend.y))) %>% 
+    ungroup() 
+  
+  bind_cols( res, 
+             lapply( res$corRes, 
+                     function(x ) data.frame( n = x$parameter, p.val = x$p.value, r = x$estimate)) %>% 
+               do.call( rbind, . ) ) %>% 
+    mutate(significant = ifelse( p.val < 0.05, '*', 'n.s.'))
+}
+
+pixel_model_cortests <- bind_rows(
+  cortest_pairs(afg_bgr), 
+  cortest_pairs(afg_pfg), 
+  cortest_pairs(afg_shr), 
+  cortest_pairs(bgr_pfg), 
+  cortest_pairs(bgr_shr), 
+  cortest_pairs(pfg_shr)
+)
+
+# Get allotment level models --------------------------------------- # 
+m_afg_allotment <- read_rds('output/AFG_cover_trend_model.rds')
+m_pfg_allotment <- read_rds('output/PFG_cover_trend_model.rds')
+m_bgr_allotment <- read_rds('output/BG_cover_trend_model.rds')
+m_shr_allotment <- read_rds('output/SHR_cover_trend_model.rds')
+
+afg_ranef2 <- format_ranef_trends(m_afg_allotment, type = 'Annual', model_scale = 'Allotment')
+pfg_ranef2 <- format_ranef_trends(m_pfg_allotment, type = 'Perennial', model_scale = 'Allotment')
+bgr_ranef2 <- format_ranef_trends(m_bgr_allotment, type = 'Bare', model_scale = 'Allotment')
+shr_ranef2 <- format_ranef_trends(m_shr_allotment, type = 'Shrub', model_scale = 'Allotment')
+
+get_fixTrends <- function( model , type ) { 
+  emtrends(model, ~ ecoregion, "year2") %>% 
+    data.frame() %>% 
+    mutate( scale = 'ecoregion', model_scale = 'Allotment', type = type) %>% 
+    rename( "ID" = ecoregion) %>%
+    rename( 'trend' = year2.trend) %>% 
+    select( type , scale, ID, trend, model_scale)
+    
+}
+
+afg_trends <- 
+  get_fixTrends(m_afg_allotment, 'Annual') %>% 
+  bind_rows( afg_ranef2) 
+
+pfg_trends <- 
+  get_fixTrends(m_pfg_allotment, 'Perennial') %>% 
+  bind_rows( pfg_ranef2) 
+
+bgr_trends <- 
+  get_fixTrends(m_bgr_allotment, 'Bare') %>% 
+  bind_rows( bgr_ranef2) 
+
+shr_trends <- 
+  get_fixTrends(m_shr_allotment, 'Shrub') %>% 
+  bind_rows(shr_ranef2) 
+
+# make pairs 
+afg_pfg2 <- make_pairwise(afg_trends, pfg_trends)
+afg_bgr2 <- make_pairwise(afg_trends, bgr_trends)
+afg_shr2 <- make_pairwise(afg_trends, shr_ranef)
+bgr_pfg2 <- make_pairwise(bgr_trends, pfg_ranef)
+bgr_shr2 <- make_pairwise(bgr_trends, shr_trends)
+pfg_shr2 <- make_pairwise(pfg_trends, shr_trends)
+
+# get correlations 
+allotment_model_cortests <- bind_rows(
+  cortest_pairs(afg_bgr2), 
+  cortest_pairs(afg_pfg2), 
+  cortest_pairs(afg_shr2), 
+  cortest_pairs(bgr_pfg2), 
+  cortest_pairs(bgr_shr2), 
+  cortest_pairs(pfg_shr2)
+)
 
 unit_scale <- function( y ) { 
   (y - min(y,na.rm = T))/diff(range(y, na.rm = T)) 
 }
+pixel_model_cortests <- bind_rows(
+  cortest_pairs(afg_bgr), 
+  cortest_pairs(afg_pfg), 
+  cortest_pairs(afg_shr), 
+  cortest_pairs(bgr_pfg), 
+  cortest_pairs(bgr_shr), 
+  cortest_pairs(pfg_shr)
+)
+all_pairs <- list(afg_bgr, afg_pfg, 
+                  afg_shr, bgr_pfg, 
+                  bgr_shr, pfg_shr, 
+                  afg_bgr2, afg_pfg2, 
+                  afg_shr2, bgr_pfg2, 
+                  bgr_shr2, pfg_shr2) %>% 
+  do.call( bind_rows , . )  %>% 
+  mutate( scale_lab = factor( scale, levels = c('ecoregion', 'ecoregion:OFFICE', 'uname', 'pixel'), ordered = T)) %>% 
+  mutate( scale_lab = factor( scale_lab, labels = c('Ecoregion', 'Office', 'Allotment', 'Pixel'))) 
+
+corplot_labels <- allotment_model_cortests %>% 
+  bind_rows(pixel_model_cortests) %>% 
+  mutate( lab = paste( "~italic(r)~'='~", round(r, 2) )) %>% 
+  mutate( lab = ifelse( p.val > 0.05, '', lab ))
+
+all_pairs <- 
+  all_pairs %>% 
+  left_join(corplot_labels %>% select( scale, pair, model_scale, p.val) ) %>% 
+  mutate( slope = ifelse(p.val < 0.05, T, F)) %>% 
+  select( -p.val)
+
+fig_seven_dat <- 
+  all_pairs %>% 
+  filter( !( model_scale == 'Pixel' & scale != 'pixel')) %>% 
+  filter( pair %in% c('Perennial_Annual', 'Bare_Annual')) %>% 
+  mutate( xlab = paste0( str_extract( pair, '[A-Za-z]+'), ' Trend')) %>% 
+  group_by( xlab, pair, model_scale, scale ) %>% 
+  mutate( across( .cols = c('trend.y', 'trend.x'), .fns = unit_scale ))
+
+fig_seven_labels <- 
+  fig_seven_dat %>%
+  ungroup() %>% 
+  distinct( xlab, scale, model_scale, pair, scale_lab ) %>% 
+  left_join(corplot_labels %>% ungroup() ) %>% 
+  select( xlab, pair, lab, model_scale, scale, scale_lab)
+
+
+letter_lab <- 
+  expand.grid( trend.y = 0, trend.x = 1, 
+               xlab = c('Bare Trend', 'Perennial Trend') , 
+               scale_lab = c('Ecoregion', 'Office', 'Allotment', 'Pixel')) %>% 
+  arrange( xlab ) %>%
+  mutate( lab = LETTERS[1:8])
+
+
+fig7 <- fig_seven_dat %>% 
+  ggplot( aes( x = trend.y, y = trend.x ) )  + 
+  geom_point(color = 'black', alpha = 0.5) +
+  geom_smooth(data = fig_seven_dat %>% filter( slope),  method = 'lm', se = F, size = 0.5) + 
+  geom_text( data = fig_seven_labels, 
+             aes( label = lab,  
+                  x = Inf, y = -Inf), 
+             hjust = 1.1, nudge_x = 0,
+             vjust = -0.5, parse = T, size = 3) + 
+  geom_text(data = letter_lab , aes( label = lab), size = 3)  + 
+  facet_grid(  xlab ~ scale_lab, switch = 'y' ) + 
+  theme_bw() +
+  coord_equal(1) + 
+  scale_x_continuous(breaks = c(0.2, 0.5, 0.8))  +
+  scale_y_continuous(breaks = c(0.2, 0.5, 0.8))  + 
+  xlab( 'Annual Cover Trend')  + 
+  #scale_x_continuous(breaks = c( 0, 0.2, 0.4, 0.6, 0.8, 1) , labels = function(x) { sprintf( "%.1f" , x )})
+  theme(strip.placement = 'outside', 
+        strip.background.y = element_blank(), 
+        axis.title.y = element_blank(), 
+        axis.text = element_text( size = 7), 
+        strip.text.y = element_text( size = 10)) 
+
+ggsave(fig7, filename = 'output/figures/Fig_7_trend_correlation.png', 
+       width = 7, height = 4, units = 'in', dpi = 600)
+
+
+make_panel <- 
+  function( sel_pair, pair_df, label_df, title_text  ) { 
+
+  figdat <- 
+    pair_df %>% 
+    filter( !( model_scale == 'Pixel' & scale != 'pixel')) %>% 
+    filter( pair == sel_pair ) %>% 
+    mutate( ylab = paste0( str_extract( pair, '[A-Za-z]+'), ' Trend')) %>% 
+    mutate( xlab = paste0( str_extract( pair, '[A-Za-z]+$'), ' Trend')) %>% 
+    group_by(xlab, ylab, pair, model_scale, scale ) %>% 
+    mutate( across( .cols = c('trend.y', 'trend.x'), .fns = unit_scale ))
+
+  fig_labels <- 
+    figdat %>%
+    ungroup() %>% 
+    distinct( xlab, ylab, scale, model_scale, pair, scale_lab ) %>% 
+    left_join(corplot_labels %>% ungroup() ) %>% 
+    select( xlab, ylab, pair, lab, model_scale, scale, scale_lab)
+  
+  xlab <- unique( figdat$xlab )
+  
+  figdat %>% 
+    ggplot( aes( x = trend.y, y = trend.x ) )  + 
+    geom_point(color = 'black', alpha = 0.5) +
+    geom_smooth(data = figdat %>% filter( slope),  method = 'lm', se = F, size = 0.5) + 
+    geom_text( data = fig_labels,
+               aes( label = lab,
+                    x = Inf, y = -Inf),
+               hjust = 1.1, nudge_x = 0,
+               vjust = -0.5, parse = T, size = 3) +
+    xlab( xlab) + 
+    facet_grid(  ylab ~ scale_lab, switch = 'y' ) + 
+    theme_bw() +
+    coord_equal(1) + 
+    scale_x_continuous(breaks = c(0.2, 0.5, 0.8))  +
+    scale_y_continuous(breaks = c(0.2, 0.5, 0.8))  + 
+    #scale_x_continuous(breaks = c( 0, 0.2, 0.4, 0.6, 0.8, 1) , labels = function(x) { sprintf( "%.1f" , x )})
+    theme(strip.placement = 'outside', 
+          strip.background.y = element_blank(), 
+          axis.title.y = element_blank(), 
+          axis.text = element_text( size = 7), 
+          strip.text.y = element_text( size = 10)) + 
+    ggtitle(title_text)
+}
+
+# First one in pair goes on y-axis 
+all_pairs$pair %>% unique
+p1 <- make_panel(sel_pair = 'Bare_Annual', all_pairs, label_df = corplot_labels, title_text = 'Annual vs. Bare')
+p2 <- make_panel(sel_pair = 'Perennial_Annual', all_pairs, label_df = corplot_labels, title_text = 'Annual vs. Perennial')
+p3 <- make_panel(sel_pair = 'Perennial_Bare', all_pairs, label_df = corplot_labels, title_text = 'Bare vs. Perennial')
+p4 <- make_panel(sel_pair = 'Perennial_Shrub', all_pairs, label_df = corplot_labels, title_text = 'Shrub vs. Perennial')
+p5 <- make_panel(sel_pair = 'Shrub_Annual', all_pairs, label_df = corplot_labels, title_text = 'Annual vs. Shrub')
+p6 <- make_panel(sel_pair = 'Shrub_Bare', all_pairs, label_df  = corplot_labels, title_text = 'Bare vs. Shrub')
+
+
+ggsave(plot = grid.arrange( p1, p2, p3, p4, p5, p6, nrow = 3, ncol = 2), 
+       filename =  'output/figures/Fig_S5_trend_correlations.png', 
+       width = 14, 
+       height = 8, 
+       units = 'in', 
+       dpi = 600)
+
+
 
 get_pairs <- function(vars = c('AFG', 'BG'), cutoff = c(0.3, 0.3)){ 
-  pixel_file <- paste0( 'data/RAP_EE_exports/', paste( tolower(vars), collapse = '_'), '_paired_samples.csv')
+  pixel_file <- paste0( 'data/old_RAP_EE_exports/', paste( tolower(vars), collapse = '_'), '_paired_samples.csv')
   pix_dat <- read_csv(pixel_file)
   col_names <- names(pix_dat) 
   
@@ -38,7 +302,7 @@ get_pairs <- function(vars = c('AFG', 'BG'), cutoff = c(0.3, 0.3)){
   
   
   out <- 
-    ecogroup_year2 %>%
+    ecoregion_year2 %>%
     bind_rows(
       office_year2 
     ) %>% 
@@ -52,7 +316,7 @@ get_pairs <- function(vars = c('AFG', 'BG'), cutoff = c(0.3, 0.3)){
         mutate( scale = 'Pixel' )
     ) %>% 
     mutate( scale = factor(scale, 
-                           levels = c('Ecogroup', 'Office', 'Allotment', 'Pixel'), 
+                           levels = c('Ecoregion', 'Office', 'Allotment', 'Pixel'), 
                            ordered = T)) %>% 
     group_by( scale ) %>%
     mutate_at( vars, unit_scale)  
@@ -60,46 +324,7 @@ get_pairs <- function(vars = c('AFG', 'BG'), cutoff = c(0.3, 0.3)){
   return(out ) 
 }
 
-make_labels <- function( res, vars = c('AFG', 'BG') ) { 
-  cortests <- res %>% 
-    group_by( scale  ) %>% 
-    summarise( res = list(cor.test(.data[[vars[1]]], .data[[vars[2]]])))
-  
-  cortests$pval <- unlist( lapply( cortests$res, FUN = function(x) x$p.value) )
-  cortests$r <- unlist( lapply( cortests$res, FUN = function(x) x$estimate) )
-  
-  lab_df <- cortests %>% 
-    select(scale, pval, r ) %>%
-    mutate( lab = paste( "~italic(r)~'='~", round(r, 2) )) %>% 
-    mutate( lab = ifelse( pval > 0.05, ' n.s.', lab ))
-  
-  return(lab_df)
-} 
 
-make_panel <- function( vars, title_text, cutoff = c(0.3,0.3) ) { 
-  
-  res <- get_pairs(vars, cutoff = cutoff ) 
-  labels = make_labels(res, vars = vars)
-  
-  res <- left_join(res, labels[, c('scale', 'pval')]) %>% 
-    mutate( dum = ifelse(pval < 0.05, T, F ))
-  
-  res %>%   
-    ggplot( aes( x = .data[[vars[1]]], y = .data[[vars[2]]] )) + 
-    geom_point(color = 'black', alpha = 0.5) + 
-    facet_grid( ~ scale ) + 
-    geom_smooth(data = res %>% filter(dum) , method = 'lm', se = F, size = 0.5) +
-    geom_text( data = labels, 
-               aes( label = lab,  
-                    x = -Inf, y = -Inf), 
-               hjust = 0, nudge_x = 1,
-               vjust = -0.5, parse = T, size = 4) + 
-    scale_x_continuous(breaks = c(0.2, 0.5, 0.8)) + 
-    scale_y_continuous(breaks = c(0.2, 0.5, 0.8)) + 
-    ggtitle(title_text ) + 
-    coord_equal(1) + 
-    theme_bw() 
-}  
 
 model_fls <- dir('output', pattern = 'cover_trend_model.rds' ,full.names =T )
 model_names <- str_extract(basename(model_fls), pattern = '[A-Za-z]+_[a-z]+')
@@ -119,9 +344,9 @@ for( i in 1:nrow( model_list )){
   temp <- lapply( temp, as.data.frame ) 
   
   temp <- format_ranefs(temp, model_list$type[i])
-  fixef_temp <- get_ecogroup_trends(read_rds(file = model_list$file[[i]]))
+  fixef_temp <- get_ecoregion_trends(read_rds(file = model_list$file[[i]]))
   fixef_temp <- data.frame( Group = names( fixef_temp), 
-              scale = 'Ecogroup', 
+              scale = 'Ecoregion', 
               type = model_list$type[i], year2 = fixef_temp  )
   
   temp <- do.call(bind_rows, temp )    
@@ -146,9 +371,9 @@ allotment_year2 <- out_all %>%
   filter( scale == 'Allotment', param == 'year2') %>% 
   pivot_wider(names_from = type, values_from = value )
 
-ecogroup_year2 <- out_all %>% 
+ecoregion_year2 <- out_all %>% 
   pivot_longer(cols=c(`(Intercept)`, year2), names_to = 'param', values_to = 'value' ) %>% 
-  filter( scale == 'Ecogroup', param == 'year2') %>% 
+  filter( scale == 'Ecoregion', param == 'year2') %>% 
   pivot_wider(names_from = type, values_from = value )
 
 p1 <- make_panel(vars = c('AFG', 'BG'), title_text = 'A) Annuals vs. Bare Ground') 
@@ -192,7 +417,7 @@ x <- x %>%
 letter_lab <- 
   expand.grid( AFG = 0, value = 1, 
                type = c('Bare Ground Trend', 'Perennial Cover Trend') , 
-               scale = c('Ecogroup', 'Office', 'Allotment', 'Pixel')) %>% 
+               scale = c('Ecoregion', 'Office', 'Allotment', 'Pixel')) %>% 
   arrange( type ) %>%
   mutate( lab = LETTERS[1:8])
 
@@ -236,13 +461,13 @@ p2$data %>% filter( scale %in% c('Allotment')) %>%
   mutate( uname = as.numeric(Group)) %>%
   left_join(a, by = 'uname') %>%
   ggplot( aes( x = AFG, y = PFG )) + 
-  geom_point() + facet_wrap( ~ ecogroup )
+  geom_point() + facet_wrap( ~ ecoregion )
 
 
 p2$data %>% filter( scale %in% c('Allotment')) %>% 
   mutate( uname = as.numeric(Group)) %>%
   left_join(a, by = 'uname') %>%
-  ggplot( aes( x = AFG, y = SHR )) + geom_point() + facet_wrap( ~ ecogroup )
+  ggplot( aes( x = AFG, y = SHR )) + geom_point() + facet_wrap( ~ ecoregion )
 
 
 p2$data %>% filter( scale %in% c('Pixel'))
--- code/analysis/A_multiscale_trend_variance.R
+++ code/analysis/A_multiscale_trend_variance.R
@@ -10,8 +10,8 @@ source('code/analysis/parameters.R')
 control_lmer$optCtrl$eval.max <- 1e9
 control_lmer$optCtrl$iter.max <- 1e9
 
-variance_analysis <- 
-  function( df, extraction_number){ 
+variance_analysis <- function( df, extraction_number){ 
+  
   mydat <- df %>% 
     select( year, uname, rep, AFG:pfgNPP, OFFICE, ecoregion) %>% 
     mutate( pixel = factor( paste(uname, rep, sep = '_'))) %>%
@@ -50,23 +50,56 @@ variance_analysis <-
   form <- value ~ t*ecoregion + (t|ecoregion:OFFICE) + (t|uname) + (t|pixel)
   
   m_afg <- lmer(data = log_AFG, form, control = control_lmer)
+  m_bgr <- lmer(data = log_BGR, form, control = control_lmer)
   m_pfg <- lmer(data = log_PFG, form, control = control_lmer)
-  m_tre <- lmer( data = log_TRE, form, control = control_lmer)
+  m_shr <- lmer(data = log_SHR, form, control = control_lmer)
+  m_tre <- lmer(data = log_TRE, form, control = control_lmer)
   
+  m_afgNPP <- lmer(data = log_afgNPP, form, control = control_lmer)
+  m_pfgNPP <- lmer(data = log_pfgNPP, form, control = control_lmer)
+  
+
+  write_rds(m_afg, paste0( 'data/temp/afg_pixel_trends_model_extraction_n', extraction_number, '.rds'))
+  write_rds(m_bgr, paste0( 'data/temp/bgr_pixel_trends_model_extraction_n', extraction_number, '.rds'))
+  write_rds(m_pfg, paste0( 'data/temp/pfg_pixel_trends_model_extraction_n', extraction_number, '.rds'))
+  write_rds(m_shr, paste0( 'data/temp/shr_pixel_trends_model_extraction_n', extraction_number, '.rds'))
+  write_rds(m_tre, paste0( 'data/temp/tre_pixel_trends_model_extraction_n', extraction_number, '.rds'))
+  write_rds(m_afgNPP, paste0( 'data/temp/afgNPP_pixel_trends_model_extraction_n', extraction_number, '.rds'))
+  write_rds(m_pfgNPP, paste0( 'data/temp/pfgNPP_pixel_trends_model_extraction_n', extraction_number, '.rds'))
   
   trend_summary <- 
     emtrends(m_afg  , ~ ecoregion, var = "t") %>% 
     data.frame()  %>%
     mutate( type = 'Annual') %>% 
     bind_rows( 
+      emtrends(m_bgr, ~ ecoregion, var = 't') %>% 
+        data.frame() %>% 
+        mutate( type = 'Bare')
+    ) %>% 
+    bind_rows( 
       emtrends(m_pfg, ~ ecoregion, var = 't') %>% 
         data.frame() %>% 
         mutate( type = 'Perennial')
     ) %>% 
     bind_rows(
+      emtrends(m_shr, ~ ecoregion, var = 't') %>% 
+        data.frame() %>%
+        mutate( type = 'Shrub')
+    ) %>%
+    bind_rows(
       emtrends(m_tre, ~ ecoregion, var = 't') %>% 
         data.frame() %>%
         mutate( type = 'Tree')
+    ) %>%
+    bind_rows(
+      emtrends(m_afgNPP, ~ ecoregion, var = 't') %>% 
+        data.frame() %>%
+        mutate( type = 'afgNPP')
+    ) %>%
+    bind_rows(
+      emtrends(m_pfgNPP, ~ ecoregion, var = 't') %>% 
+        data.frame() %>%
+        mutate( type = 'pfgNPP')
     ) 
   
   u_ecoregion <- sort( unique(trend_summary$ecoregion), decreasing = T)
@@ -74,29 +107,30 @@ variance_analysis <-
 
   trend_plot_title <- paste0('Ecoregion Trends from pixel extraction n:', extraction_number)
   
-  p1 <- trend_summary %>% 
-    mutate( type = factor(type, levels = u_type, ordered = T)) %>% 
-    mutate( ecoregion = factor(ecoregion, levels = u_ecoregion, ordered = T)) %>% 
-    ggplot( aes( color = type, x = ecoregion, y= t.trend, ymax = asymp.LCL, ymin = asymp.UCL)) + 
-    geom_point(position = position_dodge(width = 0.5)) + 
-    geom_errorbar(position = position_dodge(width = 0.5)) + 
-    geom_hline(aes( yintercept = 0)) + 
-    ggtitle(trend_plot_title) + 
-    coord_flip() + 
-    scale_color_manual(name = 'Functional Type', values = my_colors) +
-    scale_y_continuous(name = 'Trend Slope (+/- 95%CI)') +
-    scale_color_manual(name = 'Functional Type',
-                       values = my_colors,
-                       guide = guide_legend(reverse = T)) 
-  ggsave( p1, 
-          filename = paste0( 'output/figures/', trend_plot_title, '.png'), 
-          height = 8,
-          width = 7,
-          units = 'in',
-          dpi = 'print')
-  
+  write_csv( trend_summary, paste0( 'output/tables/Ecoregion_trend_summary_pixel_extraction:', 
+                                    extraction_number, '.csv'))
+  # p1 <- trend_summary %>% 
+  #   mutate( type = factor(type, levels = u_type, ordered = T)) %>% 
+  #   mutate( ecoregion = factor(ecoregion, levels = u_ecoregion, ordered = T)) %>% 
+  #   ggplot( aes( color = type, x = ecoregion, y= t.trend, ymax = asymp.LCL, ymin = asymp.UCL)) + 
+  #   geom_point(position = position_dodge(width = 0.5)) + 
+  #   geom_errorbar(position = position_dodge(width = 0.5)) + 
+  #   geom_hline(aes( yintercept = 0)) + 
+  #   ggtitle(trend_plot_title) + 
+  #   coord_flip() + 
+  #   scale_color_manual(name = 'Functional Type', values = my_colors) +
+  #   scale_y_continuous(name = 'Trend Slope (+/- 95%CI)') +
+  #   scale_color_manual(name = 'Functional Type',
+  #                      values = my_colors,
+  #                      guide = guide_legend(reverse = T)) 
+  # ggsave( p1, 
+  #         filename = paste0( 'output/figures/', trend_plot_title, '.png'), 
+  #         height = 8,
+  #         width = 7,
+  #         units = 'in',
+  #         dpi = 'print')
+  # 
   # -- Extract trend variance and plot -------------------- # 
-  var_cols <- scales::hue_pal()(3)
   extract_trend_variances <- function( model, label, measure){ 
     
     ngroups = sapply( ranef(model), nrow)
@@ -110,14 +144,32 @@ variance_analysis <-
       rename( 'scale'  = grp)
   }
   
+  extract_ranef_tables <- function( model, label, measure){ 
+    
+  ranef(model)  
+    
+  }
+  
   
   trend_variance_table <- 
     extract_trend_variances(m_afg, 'Annual', 'cover') %>% 
     bind_rows(
+      extract_trend_variances(m_bgr, 'Bare', 'cover')  
+    ) %>% 
+    bind_rows(
       extract_trend_variances(m_pfg, 'Perennial', 'cover')  
+    ) %>%     
+    bind_rows(
+      extract_trend_variances(m_shr, 'Shrub', 'cover')  
     ) %>% 
     bind_rows(
       extract_trend_variances(m_tre, 'Tree', 'cover')  
+    ) %>% 
+    bind_rows(
+      extract_trend_variances(m_afgNPP, 'AFG', 'production')  
+    ) %>% 
+    bind_rows(
+      extract_trend_variances(m_pfgNPP, 'PFG', 'production')  
     ) %>% 
     mutate( scale = str_extract(scale, '[A-Za-z]+$')) %>% 
     mutate( scale = factor(scale, levels = c('OFFICE', 'uname', 'pixel'), ordered = T)) %>% 
@@ -130,44 +182,65 @@ variance_analysis <-
     mutate( extraction_number = extraction_number)
   
   trend_variance_table %>% 
-    write_csv('output/tables/trend_variance_table.csv', append = T)
-  
-  varplot <- 
-    trend_variance_table %>% 
-    ggplot(aes( x = type_label, y = vcov, fill = scale )) + 
-    geom_bar(stat = 'identity')  + 
-    geom_text( aes( label = bar_lab, y = y_pos), size = 3) + 
-    scale_fill_manual(values = var_cols, name = 'Scale') + 
-    ylab( "Trend Variance") + 
-    theme_bw() + 
-    theme(axis.title.x = element_blank(), 
-          axis.text.x = element_text( angle = -50, hjust = 0), 
-          strip.text = element_text(hjust = 0)) 
-  
-  ggsave( varplot, 
-          filename = paste0( 'output/figures/scale_variance_plot_extraction', extraction_number, '.png'), 
-          width = 6.5, 
-          height = 4, 
-          units = 'in', dpi = 600
-          )
-  
+    write_csv(paste0('output/tables/trend_variance_table', extraction_number, '.csv'))
 }
+# 
+# df1 <- read_csv('data/RAP_EE_exports/drive-download-20220623T143258Z-001/RAP_allotment_pixel_samples1.csv') %>% 
+#   separate( `system:index`, c('year', 'allot', 'rep') , sep = '_') %>% 
+#   left_join( read_csv('data/temp/allotment_info.csv'), by = 'uname')
+# 
+# variance_analysis(df1, extraction_number = 1)
+# 
+# df2 <- read_csv('data/RAP_EE_exports/drive-download-20220623T143258Z-001/RAP_allotment_pixel_samples2.csv') %>% 
+#   separate( `system:index`, c('year', 'allot', 'rep') , sep = '_') %>% 
+#   left_join( read_csv('data/temp/allotment_info.csv'), by = 'uname')
+# 
+# variance_analysis(df2, extraction_number = 2)
+# 
+# df3 <- read_csv('data/RAP_EE_exports/drive-download-20220623T143258Z-001/RAP_allotment_pixel_samples3.csv') %>% 
+#   separate( `system:index`, c('year', 'allot', 'rep') , sep = '_') %>% 
+#   left_join( read_csv('data/temp/allotment_info.csv'), by = 'uname')
+# 
+# variance_analysis(df3, extraction_number = 3)
+# 
+# df6 <- read_csv('data/RAP_EE_exports/drive-download-20220624T013905Z-001/RAP_allotment_pixel_samples6.csv') %>% 
+#   separate( `system:index`, c('year', 'allot', 'rep') , sep = '_') %>% 
+#   left_join( read_csv('data/temp/allotment_info.csv'), by = 'uname')
+# 
+# variance_analysis(df6, extraction_number = 6)
+# 
 
-df1 <- read_csv('data/RAP_EE_exports/drive-download-20220623T143258Z-001/RAP_allotment_pixel_samples1.csv') %>% 
+df4 <- read_csv('data/RAP_EE_exports/drive-download-20220624T051641Z-001/RAP_allotment_pixel_samples4.csv') %>% 
   separate( `system:index`, c('year', 'allot', 'rep') , sep = '_') %>% 
   left_join( read_csv('data/temp/allotment_info.csv'), by = 'uname')
 
-variance_analysis(df1, extraction_number = 1)
+variance_analysis(df4, extraction_number = 4)
 
-df2 <- read_csv('data/RAP_EE_exports/drive-download-20220623T143258Z-001/RAP_allotment_pixel_samples2.csv') %>% 
-  separate( `system:index`, c('year', 'allot', 'rep') , sep = '_') %>% 
-  left_join( read_csv('data/temp/allotment_info.csv'), by = 'uname')
+trend_variance_table <- read_csv('output/tables/trend_variance_table4.csv')
+var_cols <- scales::hue_pal()(3)
 
-variance_analysis(df2, extraction_number = 2)
+trend_variance_table <- 
+  trend_variance_table %>% 
+  mutate( scale = factor(scale, levels = c('office', 'allotment', 'pixel'), ordered = T))
 
-df3 <- read_csv('data/RAP_EE_exports/drive-download-20220623T143258Z-001/RAP_allotment_pixel_samples3.csv') %>% 
-  separate( `system:index`, c('year', 'allot', 'rep') , sep = '_') %>% 
-  left_join( read_csv('data/temp/allotment_info.csv'), by = 'uname')
+varplot <- 
+  trend_variance_table %>% 
+  filter( extraction_number == 4) %>% 
+  ggplot(aes( x = type_label, y = vcov, fill = scale )) + 
+  geom_bar(stat = 'identity')  + 
+  geom_text( aes( label = bar_lab, y = y_pos), size = 3) + 
+  scale_fill_manual(values = var_cols, name = 'Scale') + 
+  ylab( "Trend Variance") + 
+  theme_bw() + 
+  theme(axis.title.x = element_blank(), 
+        axis.text.x = element_text( angle = -50, hjust = 0), 
+        strip.text = element_text(hjust = 0)) 
 
-variance_analysis(df3, extraction_number = 3)
 
+
+ggsave( varplot, 
+        filename = paste0( 'output/figures/scale_variance_plot_extraction', extraction_number, '.png'), 
+        width = 6.5, 
+        height = 4, 
+        units = 'in', dpi = 600
+)
